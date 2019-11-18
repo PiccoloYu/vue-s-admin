f73d0853ff74e19798c97b82ca18384cc37f261e
piccoloyu <1490169007@qq.com>
2019-11-18 18:15:02 +0800

    搭建 pages

--- docs/js/chunk-2d22db84.b4a92ccd.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22db84"],{f961:function(e,r,n){"use strict";n.r(r);n("e57a"),n("85dc");var t,u,a={created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("4e82"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d22db84.b4a92ccd.js.map
