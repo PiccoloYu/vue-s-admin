81021bbecc4c507af65491d4cedea2109a2a8746
PiccoloYu <44945137+PiccoloYu@users.noreply.github.com>
2019-12-09 09:31:36 +0000

    Deploying to gh-pages from master 0b63310550e7dc874d3d3843af30fcb01cc7b9e1

---
+++ js/chunk-2d22db84.d0a7c4f1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22db84"],{f961:function(e,r,n){"use strict";n.r(r);n("5319");var t,u,a={created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("2877"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d22db84.d0a7c4f1.js.map
